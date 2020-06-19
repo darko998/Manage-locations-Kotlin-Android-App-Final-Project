df76f997c932c194de40dac23651ea6068986489
darko998 <darko.dardi@gmail.com>
2020-06-19 13:02:45 +0200

    Initial commit.

---
+++ app/src/main/java/rs/raf/projekat2/darko_dimitrijevic_rn9418/MainActivity.kt
@@ -0,0 +1,12 @@
+package rs.raf.projekat2.darko_dimitrijevic_rn9418
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
